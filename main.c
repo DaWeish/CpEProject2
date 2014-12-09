322362cd58e043f933df2be4661ccc8f0c323441
Connor Walsh <cwd8d@mst.edu>
2014-12-09 13:23:05 -0600

    add definitions for timer interrupts, need to fill in functions

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include "reg932.h"
 #include "uart.h"
 
+#define OSCFREQ 7372800
+
+sbit speaker = P1^7;
+
+void timer1_tone(void) interrupt 3 using 3;
+
 void main()
 {
-	
+	// Test playing a few note song
+}
+
+void timer1_tone(void) interrupt 3 using 3
+{
+	TH1 = -((*note_ptr) >> 8);
+	TL1 = -((*note_ptr) & 0x00ff);
+	speaker = ~speaker;
+}
+
+void timer0_durr(void) interrupt 1 using 3 // need to plan out better
+{
+	if (timeLeft)
+		timeLeft -= 1;
+
+	else
+	{
+		currNote += 1;
+		if (currNote >= songSize)
+			currNote = 0;
+		
+		
+		
+	}
 }
