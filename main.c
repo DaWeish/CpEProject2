902638637776de3aa81e5d55bafb6ef8caec0315
Connor Walsh <cwd8d@mst.edu>
2014-12-09 17:59:24 -0600

    add note_period header file to hold values for playing notes

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include "reg932.h"
 #include "uart.h"
 
+#include "note_periods.h"
+
 #define OSCFREQ 7372800
 
+// Tempo is beats per minute
+#define TEMPO 60
+#define NOTE_DUR_FACTOR ((((60/TEMPO)/16)/177)*10000) 
+
 sbit speaker = P1^7;
+
+unsigned char* note_ptr;
+unsigned char* durr_ptr;
+
+unsigned char songSize = 0;
+unsigned char currNote = 0;
+
+unsigned int noteTime = 0;
+unsigned 
 
 void timer1_tone(void) interrupt 3 using 3;
 
@@ -14,23 +29,10 @@ void main()
 
 void timer1_tone(void) interrupt 3 using 3
 {
-	TH1 = -((*note_ptr) >> 8);
-	TL1 = -((*note_ptr) & 0x00ff);
-	speaker = ~speaker;
+	
 }
 
 void timer0_durr(void) interrupt 1 using 3 // need to plan out better
 {
-	if (timeLeft)
-		timeLeft -= 1;
-
-	else
-	{
-		currNote += 1;
-		if (currNote >= songSize)
-			currNote = 0;
-		
-		
-		
-	}
+	
 }
---
+++ note_periods.h
@@ -0,0 +1,26 @@
+#ifndef NOTE_PERIODS_H
+#define NOTE_PERIODS_H
+
+code int notes[] = {0,C4,D4,E4,F4,G4,A4,B4,C5};
+
+#define E3
+#define F3
+#define G3
+#define Ab3
+#define A3
+#define Bb3
+#define B3
+#define C4 7046
+#define Db4 6640
+#define D4 6276
+#define Eb4 5924
+#define E4 5592
+#define Gb4 4982
+#define G4 4702
+#define Ab4 4438
+#define A4 4189
+#define Bb4 3954
+#define B4 3732
+#define C5 3523
+
+#endif
